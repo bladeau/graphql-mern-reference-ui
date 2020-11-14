aeb066e47337aaea7c5af783454b3b730dca9e87
Al <al.almaktoum@live.com>
2020-11-14 11:42:51 +1100

    useContext to render state and dispatch change to reducer onClick

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { AuthProvider } from './context/authContext'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import { gql, useQuery, useLazyQuery } from '@apollo/client'
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const GET_ALL_POSTS = gql`
   {
@@ -13,6 +15,16 @@ const GET_ALL_POSTS = gql`
 const Home = () => {
   const { loading, error, data } = useQuery(GET_ALL_POSTS)
   const [fetchPosts, { data: posts }] = useLazyQuery(GET_ALL_POSTS)
+
+  // access context
+  const { state, dispatch } = useContext(AuthContext)
+
+  const updateUserName = () => {
+    dispatch({
+      type: 'LOGGED_IN_USER',
+      payload: 'Al',
+    })
+  }
 
   if (loading) return <p className='p-5'>Loading...</p>
   if (error) return <p className='p-5'>Error :(</p>
@@ -35,13 +47,20 @@ const Home = () => {
       <div className='row p-5'>
         <button
           onClick={() => fetchPosts()}
-          className='btn-btn-raised btn-primary'
+          className='btn btn-raised btn-primary'
         >
           Fetch Posts
         </button>
       </div>
-      <hr />
+
       {JSON.stringify(posts)}
+      <hr />
+      {JSON.stringify(state.user)}
+
+      <hr />
+      <button className='btn btn-primary' onClick={updateUserName}>
+        Change User name
+      </button>
     </div>
   )
 }
